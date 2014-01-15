da6fa62394a20c4b8a27e62a7e81e4489cbd12b8
janvillarosa <janrobertvillarosa@gmail.com>
2014-01-15 20:56:36 +0800

    Added bin and gen folders to Repo

---
+++ Echonotes/gen/com/mobicom/echonotes/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.mobicom.echonotes;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@ echonotes
 =========
 
 This is a github repository for a note taking service called Echonotes.
+New repo, now 100% cleaner!
